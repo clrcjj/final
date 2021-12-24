3fcf55bd4da17e0343472a49f04cb83aec2bce51
clrcjj <caijingjing2001@163.com>
2021-12-24 04:29:01 -0800

    mongo连接

---
+++ utils/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	project: {
+		prot: 27017,
+		host: '127.0.0.1',
+		dbName: '190110910802',
+	},
+	sessToken: {
+		prot: 27017,
+		host: '127.0.0.1',
+		dbName: '190110910802',
+		collection: 'session2',
+		maxAge: 86400,
+	},
+	sessCookie: {
+		prot: 27017,
+		host: '127.0.0.1',
+		dbName: '190110910802',
+		collection: 'session1',
+		maxAge: 86400,
+	}
+}
