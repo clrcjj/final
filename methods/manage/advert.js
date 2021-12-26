d698cef20ead312afb5503fc18daa699cd63350b
clrcjj <caijingjing2001@163.com>
2021-12-25 22:24:22 -0800

    advert

--- methods/manage/advert.js
+++ methods/manage/advert.js
@@ -113,7 +113,7 @@ let DirAdvertImgs = async (ctx, next) => {
 
 }
 // 上传图片
-let Images = async (ctx, next) => {
+let UploadImages = async (ctx, next) => {
 
 	await authToken(ctx, next, async () => {
 
